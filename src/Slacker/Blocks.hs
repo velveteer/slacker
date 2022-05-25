bec40c6ee67692ce9d2cdca049afb2c655d196bc
Josh Miller <miller.joshua@heb.com>
2022-05-25 09:50:11 -0500

    clean up blocks module

--- src/Slacker/Blocks.hs
+++ src/Slacker/Blocks.hs
@@ -1,56 +1,12 @@
 module Slacker.Blocks
-  ( -- * Layout blocks
-    Blocks
-  , HeaderBlock
-  , header
-  , header_
-  , DividerBlock
-  , divider
-  , divider_
-  , ActionsBlock
-  , actions
-  , actions_
-  , ContextBlock
-  , ContextElement
-  , context
-  , context_
-  , contextImage
-  , contextImage_
-  , contextText
-  , SectionBlock
-  , section
-  , section_
-  , sectionNoText
-  , sectionNoText_
-  , Fields
-  , field
-  , ImageBlock
-  , image
-  , image_
-  -- * Text objects
-  , TextObject
-  , markdown
-  , plaintext
-  , embolden
-  , italicize
-    -- * Elements
-  , Elements
-  , ButtonElement
-  , button
-  , button_
-  , ImageElement
-  , imageElement
-  , imageElement_
-  , module Named
+  ( module Export
   ) where
 
-import Slacker.Blocks.Actions
-import Slacker.Blocks.Context
-import Slacker.Blocks.Core
-import Slacker.Blocks.Divider
-import Slacker.Blocks.Header
-import Slacker.Blocks.Image
-import Slacker.Blocks.Section
-import Slacker.Blocks.Elements
-import Named
-
+import Slacker.Blocks.Actions as Export
+import Slacker.Blocks.Context as Export
+import Slacker.Blocks.Core as Export
+import Slacker.Blocks.Divider as Export
+import Slacker.Blocks.Header as Export
+import Slacker.Blocks.Image as Export
+import Slacker.Blocks.Section as Export
+import Slacker.Blocks.Elements as Export
